6e025c18a14ebf75f937bec3fb077aac19aa92c4
MarioWintes <52406123@edu.campus02.at>
2024-09-21 17:54:04 +0200

    sumApp completed

--- src/NumberSumApp.java
+++ src/NumberSumApp.java
@@ -3,12 +3,11 @@ public class NumberSumApp {
 
         int i = 0;
         int sum = 0;
-        
+
         while (i < 100) {
             i = i + 1;
             sum = sum + i;
-            System.out.println(i);
         }
-        System.out.println(sum);
+        System.out.println("Die Gesamtsumme von 1-100 ist: " + sum);
     }
 }
